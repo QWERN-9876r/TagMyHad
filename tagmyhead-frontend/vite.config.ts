17f13ba0410818235f7a894e90c0e07ebbe300d4
Qwern9876r <dimadobriy111@gmail.com>
2025-11-05 14:21:22 +0300

    bug fix

--- tagmyhead-frontend/vite.config.ts
+++ tagmyhead-frontend/vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    server: {
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true,
+    },
+    preview: {
+        host: '0.0.0.0',
+        allowedHosts: ['dimalovedasha.ru'],
         port: 80,
+    },
+    server: {
         proxy: {
             '/api': {
                 target: 'http://89.169.151.181:8080',
