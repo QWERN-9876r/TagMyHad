4c66f11aaab5e4976ba06bc292bbc4c3eb802458
Qwern9876r <dimadobriy111@gmail.com>
2025-11-05 20:01:58 +0300

    bug fix

--- tagmyhead-frontend/vite.config.ts
+++ tagmyhead-frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     },
     preview: {
         host: '0.0.0.0',
-        allowedHosts: ['dimalovedasha.ru'],
+        allowedHosts: ['dimalovedasha.ru', 'tagmyhead.online', 'tagmyhead.ru'],
         port: 80,
     },
     server: {
